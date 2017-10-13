ec702afb994ecee3e15ec11c4c7ef753b70c7d09
gf <gaofen_888@163.com>
2017-10-13 17:24:39 +0800

    部分优化

--- web/src/main/webapp/plug-in/layim/config.js
+++ web/src/main/webapp/plug-in/layim/config.js
@@ -16,8 +16,7 @@ if(!/^http(s*):\/\//.test(location.href)){
 if (typeof WebSocket != 'undefined') {
 layui.use('layim', function(layim){
 //      //建立WebSocket通讯
-    var socket = new WebSocket("ws://"+chatIp+"/jeecg/WebSocket/"+id);
-
+    var socket = new WebSocket("ws://"+chatIp+"/platform/WebSocket/"+id);
     //基础配置
     layim.config({
         msgbox: layui.cache.dir + 'css/modules/layim/html/msgbox.html',
